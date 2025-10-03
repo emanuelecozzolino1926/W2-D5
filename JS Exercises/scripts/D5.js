e61dce6818a7e43b812fc672ddc67deec6d37882
emanuelecozzolino1926 <emanuele.cozzolino18@gmail.com>
2025-10-03 15:06:23 +0200

    [UPD]First javascript exercise completed

--- JS Exercises/scripts/D5.js
+++ JS Exercises/scripts/D5.js
@@ -12,6 +12,13 @@ REGOLE
 */
 const pets = ['dog', 'cat', 'hamster', 'redfish']
 
+console.log(pets) // cosi stampo array
+
+//con questo ciclo for stamo ogni contenuto del array
+for(let i = 0; i < pets.length; i++){
+  console.log(pets[i])
+}
+
 /* ESERCIZIO 2
     Scrivi del codice per ordinare alfabeticamente gli elementi dell'array "pets".
 */
